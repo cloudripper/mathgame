d560617afa07d02bd22d1eb623940b677242f351
Tim <tim@verticalwild.com>
2020-11-03 14:29:24 -0800

    Completed general layout/styling && Added Underscore && Beginning JS

---
+++ js/application.js
@@ -0,0 +1,18 @@
+$('document').ready(function () {
+
+});
+
+var numRange = _.range(0,100);
+
+var problem = function () {
+    var a = _.sample(numRange, 1);
+    var b = _.sample(numRange, 1);
+    var operator = "+";
+    var probProcess = a 
+
+    while (a )
+
+};
+
+
+console.log(numRange);
